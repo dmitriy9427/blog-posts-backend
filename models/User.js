e4b03e79584897bd388fd81b02c9aada0071fa14
Dima Ryabov <dima.poltergeist.2015@yandex.ru>
2023-10-06 15:44:50 +0300

    shifr

--- models/User.js
+++ models/User.js
@@ -6,6 +6,10 @@ const userModel = new Schema(
     username: {
       type: String,
       required: true,
+    },
+    email: {
+      type: String,
+      required: true,
       unique: true,
     },
     password: {
